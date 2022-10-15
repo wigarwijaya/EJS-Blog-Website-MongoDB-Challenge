e58d5af43a25347db41cef46a44f098ec366cad8
wigarwijaya <wigarwijaya@gmail.com>
2022-10-15 18:14:42 +0700

    done

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const _ = require("lodash")
-const mongoose = require("mongoose");
-const port = 3000;
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,67 +16,65 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB");
-const { Schema } = mongoose;
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-const array = require('lodash/array');
-const object = require('lodash/fp/object');
-const { ListCollectionsCursor } = require("mongodb");
-
-// -----------------------------------------------------------------------------
-// DB Schema
-
-const postSchema = new Schema({
+const postSchema = {
   title: String,
   content: String
-});
+};
 
 const Post = mongoose.model("Post", postSchema);
 
-app.get("/", (req, res) => {
-  Post.find({}, function(err, docs){
-        res.render("home", { startingContent: homeStartingContent, newPost: docs });
-  })
-})
+app.get("/", function(req, res){
 
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
 
-app.get("/about", (req, res) => {
-  res.render("about", { aboutContent: aboutContent })
-})
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
-app.get("/contact", (req, res) => {
-  res.render("contact", { contactContent: contactContent })
-})
-
-app.get("/compose", (req, res) => {
-  res.render("compose")
-})
-
-app.post("/compose", (req, res) => {
+app.post("/compose", function(req, res){
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
+
   post.save(function(err){
     if (!err){
-      res.redirect("/");
+        res.redirect("/");
     }
   });
 });
 
-app.get("/post/:postId", (req, res) => {
-  const requestedPostId = req.params.postId;
+app.get("/posts/:postId", function(req, res){
 
-  Post.findOne({ _id: requestedPostId }, function (err, post){
-    res.render("post", { title: post.title, content: post.content })
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
 
 
-app.listen(port, function() {
-  console.log(`Server started on port ${port}`);
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
-
-// NOTES
-// beberapa kali error ternyata pas node versionnya diturunin (jadi v14.20.0) langsung bisa
